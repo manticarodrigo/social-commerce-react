ec8e8e74c880807e83c3ebb126e7e36b1becaadd
manticarodrigo <manticarodrigo@gmail.com>
2018-07-16 20:00:13 -0400

    changed app theme colors and logo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ED7C35',
+      contrastText: '#FFF'
+    }
+  },
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <MuiThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MuiThemeProvider>,
   document.getElementById('root')
 )
 registerServiceWorker()
