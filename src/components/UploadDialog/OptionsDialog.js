d5feac453db318f536e4b28e8c4254f94d31c79c
manticarodrigo <manticarodrigo@gmail.com>
2018-08-09 15:01:01 -0400

    options dialog stateless component

--- src/components/UploadDialog/OptionsDialog.js
+++ src/components/UploadDialog/OptionsDialog.js
@@ -1,42 +1,38 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Dialog from '@material-ui/core/Dialog'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-class OptionsDialog extends Component {
+const OptionsDialog = ({ onClose, open, options }) => {
 
-  handleClose = () => {
-    this.props.onClose()
+  const handleClose = () => {
+    onClose();
   }
 
-  handleListItemClick = (value) => {
-    this.setState({ open: false })
-    this.props.onClose(value)
+  const handleListItemClick = (value) => {
+    onClose(value);
   }
-
-  render() {
-    const { open, options } = this.props
-    return (
-      <Dialog
-        open={open}
-        onClose={this.handleClose}
-        aria-labelledby="options-dialog-title">
-        <DialogTitle id="options-dialog-title">Suba Imagen</DialogTitle>
-        <div>
-          <List>
-            {options.map(option => (
-              <ListItem button onClick={() => this.handleListItemClick(option)} key={option}>
-                <ListItemText primary={option} />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      </Dialog>
-    )
-  }
+  
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="options-dialog-title">
+      <DialogTitle id="options-dialog-title">Suba Imagen</DialogTitle>
+      <div>
+        <List>
+          {options.map(option => (
+            <ListItem button onClick={() => handleListItemClick(option)} key={option}>
+              <ListItemText primary={option} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    </Dialog>
+  )
 }
 
 OptionsDialog.propTypes = {
